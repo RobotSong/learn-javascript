0d26b25fa046b6635fba19434615b2f202585e39
cyg <m15877032720@163.com>
2022-02-17 13:42:27 +0800

    react-route 基本路由的使用

--- learn-react/11_路由router/01_base_router/src/App.js
+++ learn-react/11_路由router/01_base_router/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Link, Route, Routes } from 'react-router-dom'
+import {Link, Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 
@@ -13,28 +13,28 @@ class App extends Component {
                     </div>
                 </div>
 
-                    <div className="row">
-                        <div className="col-3">
-                            {/* 在多页面应用中，通过 a 标签的 href 来切换不同的页面 */}
-                            <div className="nav flex-column nav-pills p-3 border rounded">
-                                {/*    <a className="nav-link active" href="./home.html" >Home</a>*/}
-                                {/*    <a className="nav-link" href="./about.html" >About</a>*/}
-                                {/* 在单页应用中， 靠 路由连接（Link） 来实现切换不同的组件，路由连接要被路由器包裹才能使用   */}
-                                {/*   编写路由连接 */}
-                                <Link className="nav-link" to={'/home'} >Home</Link>
-                                <Link className="nav-link" to={'/about'}>About</Link>
-                            </div>
-                        </div>
-                        <div className="col-9">
-                            <div className="p-3 border rounded h-100">
-                                {/* 注册路由 */}
-                                <Routes>
-                                    <Route path={'/home'} element={<Home />} />
-                                    <Route path={'/about'} element={<About />} />
-                                </Routes>
-                            </div>
+                <div className="row">
+                    <div className="col-3">
+                        {/* 在多页面应用中，通过 a 标签的 href 来切换不同的页面 */}
+                        <div className="nav flex-column nav-pills p-3 border rounded">
+                            {/*    <a className="nav-link active" href="./home.html" >Home</a>*/}
+                            {/*    <a className="nav-link" href="./about.html" >About</a>*/}
+                            {/* 在单页应用中， 靠 路由连接（Link） 来实现切换不同的组件，路由连接要被路由器包裹才能使用   */}
+                            {/*   编写路由连接 */}
+                            <Link className="nav-link" to={'/home'}>Home</Link>
+                            <Link className="nav-link" to={'/about'}>About</Link>
                         </div>
                     </div>
+                    <div className="col-9">
+                        <div className="p-3 border rounded h-100">
+                            {/* 注册路由 */}
+                            <Routes>
+                                <Route path={'/home'} element={<Home/>}/>
+                                <Route path={'/about'} element={<About/>}/>
+                            </Routes>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
