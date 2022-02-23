fd8e5ea6cce4b77e1317e8a2015561789149053d
cyg <m15877032720@163.com>
2022-02-23 18:08:55 +0800

    添加 antd 下拉菜单的学习

--- learn-react/12_UI组件库antd/01_use/src/App.js
+++ learn-react/12_UI组件库antd/01_use/src/App.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-import { Button, Space, DatePicker } from 'antd';
+import { Button, Space, DatePicker, Select } from 'antd';
 import { WechatOutlined, SearchOutlined } from '@ant-design/icons';
 // import 'antd/dist/antd.css'; //
 
 const { RangePicker  } = DatePicker
+const { Option } = Select;
+const children = [];
+for (let i = 10; i < 36; i++) {
+    children.push(<Option key={i.toString(36) + i} value={i.toString()}>{i.toString(36) + i}</Option>);
+}
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
+}
 
 class App extends Component {
 
@@ -13,33 +22,40 @@ class App extends Component {
 
     render() {
         return (
-            <Space>
-                App...
-                <Button type="primary" loading={true} >Primary Button</Button>
-                <Button>Default Button</Button>
-                <Button type="dashed">Dashed Button</Button>
-                <br />
-                <Button type="text">Text Button</Button>
-                <Button type="link">Link Button</Button>
-                <WechatOutlined spin={true} />
-                <DatePicker onChange={this.onChange} />
-                <RangePicker
-                    dateRender={current => {
-                    const style = {};
-                    if (current.date() === 1) {
-                        style.border = '1px solid #1890ff';
-                        style.borderRadius = '50%';
-                    }
-                    return (
-                        <div className="ant-picker-cell-inner" style={style}>
-                            {current.date()}
-                        </div>
-                    );
-                    }} />
-                <Button type="primary" icon={<SearchOutlined />}>
-                    Search
-                </Button>
-            </Space>
+            <div>
+                <Space>
+                    <Button type="primary" loading={true} >Primary Button</Button>
+                    <Button>Default Button</Button>
+                    <Button type="dashed">Dashed Button</Button>
+                    <br />
+                    <Button type="text">Text Button</Button>
+                    <Button type="link">Link Button</Button>
+                    <WechatOutlined spin={true} />
+                    <DatePicker onChange={this.onChange} />
+                    <RangePicker
+                        dateRender={current => {
+                            const style = {};
+                            if (current.date() === 1) {
+                                style.border = '1px solid #1890ff';
+                                style.borderRadius = '50%';
+                            }
+                            return (
+                                <div className="ant-picker-cell-inner" style={style}>
+                                    {current.date()}
+                                </div>
+                            );
+                        }} />
+                    <Button type="primary" icon={<SearchOutlined />}>
+                        Search
+                    </Button>
+
+                </Space>
+                <div>
+                    <Select mode="tags" style={{ width: 200 }} placeholder="Tags Mode" onChange={handleChange}>
+                        {children}
+                    </Select>
+                </div>
+            </div>
         );
     }
 }
